8cdd12cde8f60fe5a23d8bfe45c025df05182f48
Mayara Almeida de Sousa <may.almeida.de.sousa@gmail.com>
2022-06-15 23:04:32 -0300

    Novo Projeto
    Upando o novo projeto para o repositório.

---
+++ Listas e Strings/Strings e Listas.py
@@ -0,0 +1,30 @@
+#Projeto desenvolvido para o entendimento dos conceitos básicos de Strings e Listas.
+#Paso1: Inserção do nome.
+print("------------ Brincando com Strings --------------")
+nome=input("Insira o seu Nome : ")
+#Passo2:Mostrar cada letra armazenada na variável.
+#Limitação: Só funcinará com nomes que possuem 6 letras.
+print(nome)
+print(nome[0])
+print(nome[1])
+print(nome[2])
+print(nome[3])
+print(nome[4])
+print(nome[5])
+#Passo3:Mostrar ao usuário a quantidade de letras que o nome possue.
+print("A Quantidade de letras é ",len(nome))
+#Fim da primeira parte.
+#Início da segunda parte com Listas.
+#Passo1:Inserção dos nomes dentro das listas.
+#Limitação: A mesma da primeira parte só funcionará com a quantidade pré formatada.
+print("------------ Brincando com Listas --------------------")
+Avang_nome=["Steve Rogers","Tony Stark","Peter Parker","Clint Barneers","Thor Odinson"]
+#Passo2:Mostrar cada nome para o usuário.
+print(Avang_nome[0])
+print(Avang_nome[1])
+print(Avang_nome[2])
+print(Avang_nome[3])
+print(Avang_nome[4])
+#Passo3: Mostar a quantidade de elementos da lista, ou seja, o tamanho da lista e não a quantidade de letras de cada nome.
+#isto é o que diferencia um funcção "LEN" da outra.
+print("A Quantidade de elementos é ",len(Avang_nome))
